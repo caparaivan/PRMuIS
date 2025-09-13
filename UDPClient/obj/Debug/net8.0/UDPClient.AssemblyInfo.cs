8d8d3b3ed94e7353f035d961a93a597614c3edb0
caparaivan <capara.ivan359@gmail.com>
2025-09-13 14:09:39 +0200

    a
    a

--- UDPClient/obj/Debug/net8.0/UDPClient.AssemblyInfo.cs
+++ UDPClient/obj/Debug/net8.0/UDPClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UDPClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2877893ad377577f0bf7c43fcefaee66707c049a")]
 [assembly: System.Reflection.AssemblyProductAttribute("UDPClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UDPClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
